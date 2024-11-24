7638a543e916df03e614e730eccb4a06b987b4e3
SanderNKL <sander.lastad@gmail.com>
2024-11-24 03:29:31 +0100

    fixed spacing

--- javascript/tests/list.test.js
+++ javascript/tests/list.test.js
@@ -8,6 +8,6 @@ describe("arrayOfAnimals", () => {
 	test("Test that we can append animals", () => {
         const animals = ['cat'];
 		expect(arrayOfAnimals(animals, 'dog')).toStrictEqual(['cat', 'dog']);
-        expect(arrayOfAnimals(animals, 'horse')).toStrictEqual(['cat', 'dog', 'horse']);
+		expect(arrayOfAnimals(animals, 'horse')).toStrictEqual(['cat', 'dog', 'horse']);
 	});
 });
